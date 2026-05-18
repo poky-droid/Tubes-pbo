1e0f68532fa490fbbc8a05b40fca25e85c01d37d
abdad mubaroq <abdadmubaroq@MacBook-Air-abdad.local>
2026-05-18 13:39:36 +0700

    login v0.1

--- src/main/java/com/example/demo/controller/userController.java
+++ src/main/java/com/example/demo/controller/userController.java
@@ -1,7 +1,26 @@
 package com.example.demo.controller;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import com.example.demo.model.user;
-
+@Controller
 public class userController  extends user {
-
+    @RequestMapping(method=RequestMethod.GET, value = "/")
+    public String home() {
+        return "index";
+    }
+    
+    @RequestMapping(method = RequestMethod.GET, value = "/login form")
+    public String loginController(@RequestParam("email") String email, @RequestParam("password") String password) {
+        if (login(email, password)) {
+            return "home";
+        } else {
+            return "Login gagal!";
+        }
+    }
 }
+
--- src/main/java/com/example/demo/model/user.java
+++ src/main/java/com/example/demo/model/user.java
@@ -1,5 +1,22 @@
 package com.example.demo.model;
 
 public class user {
+    protected int idUser;
+    protected String nama;
+    protected String email;
+    protected String password;
+
+    public user() {
+    }
+
+    public boolean login(String email, String password) {
+
+
+        return true; 
+    }
+
+
+
+    
 
 }
