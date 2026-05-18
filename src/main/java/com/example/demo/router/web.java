2d89ead387e6f48a84e119f9d66f43ffbfd56db8
abdad mubaroq <abdadmubaroq@MacBook-Air-abdad.local>
2026-05-18 09:06:27 +0700

    routing dan login page

--- src/main/java/com/example/demo/router/web.java
+++ src/main/java/com/example/demo/router/web.java
@@ -1,5 +1,15 @@
 package com.example.demo.router;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class web {
+
+    @RequestMapping(method = RequestMethod.GET, value = "/")
+    public String webRoot(){
+        return "index";
+    }
 
 }
