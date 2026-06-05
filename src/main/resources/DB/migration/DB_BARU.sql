d54258e2889d87858f570d158f67c8c550d60f79
wilfaiz <wildanfilzaalfaiz@gmail.com>
2026-06-06 00:01:01 +0700

    bisa 95% (NEW DB)

---
+++ src/main/resources/DB/migration/DB_BARU.sql
@@ -0,0 +1,358 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Jun 05, 2026 at 04:59 PM
+-- Server version: 8.0.42
+-- PHP Version: 8.3.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tubes_pbo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kendaraan`
+--
+
+CREATE TABLE `kendaraan` (
+  `id_kendaraan` bigint NOT NULL,
+  `merk` varchar(100) NOT NULL,
+  `model` varchar(100) NOT NULL,
+  `tahun` year NOT NULL,
+  `harga` decimal(15,2) NOT NULL,
+  `status` varchar(50) NOT NULL,
+  `foto` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `kendaraan`
+--
+
+INSERT INTO `kendaraan` (`id_kendaraan`, `merk`, `model`, `tahun`, `harga`, `status`, `foto`) VALUES
+(8, 'esemka', 'aerox', '2024', 25000000.00, 'Terjual', 'f2e6105c-7cc5-4dc4-928f-c4153d1405c3.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mobil`
+--
+
+CREATE TABLE `mobil` (
+  `id_mobil` bigint NOT NULL,
+  `id_kendaraan` bigint NOT NULL,
+  `mesin_mobil` varchar(100) DEFAULT NULL,
+  `jenis_mobil` varchar(100) DEFAULT NULL,
+  `transmisi_mobil` varchar(50) DEFAULT NULL,
+  `kapasitas_mobil` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `mobil`
+--
+
+INSERT INTO `mobil` (`id_mobil`, `id_kendaraan`, `mesin_mobil`, `jenis_mobil`, `transmisi_mobil`, `kapasitas_mobil`) VALUES
+(6, 8, '-', '-', '-', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `motor`
+--
+
+CREATE TABLE `motor` (
+  `id_motor` bigint NOT NULL,
+  `id_kendaraan` bigint NOT NULL,
+  `cc` int DEFAULT NULL,
+  `jenis_motor` varchar(100) DEFAULT NULL,
+  `kapasitas_tangki` decimal(5,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `owner`
+--
+
+CREATE TABLE `owner` (
+  `id_owner` bigint NOT NULL,
+  `id_user` bigint NOT NULL,
+  `kontak` varchar(20) DEFAULT NULL,
+  `kota` varchar(100) DEFAULT NULL,
+  `provinsi` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `owner`
+--
+
+INSERT INTO `owner` (`id_owner`, `id_user`, `kontak`, `kota`, `provinsi`) VALUES
+(1, 4, '0812-9999-8888', 'Tasikmalaya', 'Jawa Barat');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pembeli`
+--
+
+CREATE TABLE `pembeli` (
+  `id_pembeli` bigint NOT NULL,
+  `id_user` bigint NOT NULL,
+  `kontak` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `pembeli`
+--
+
+INSERT INTO `pembeli` (`id_pembeli`, `id_user`, `kontak`) VALUES
+(2, 2, '0323103232'),
+(3, 3, '0323103232');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `penjualan`
+--
+
+CREATE TABLE `penjualan` (
+  `id_penjualan` bigint NOT NULL,
+  `tanggal` date NOT NULL,
+  `status` varchar(50) NOT NULL,
+  `id_pembeli` bigint NOT NULL,
+  `id_kendaraan` bigint NOT NULL,
+  `id_owner` bigint DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `penjualan`
+--
+
+INSERT INTO `penjualan` (`id_penjualan`, `tanggal`, `status`, `id_pembeli`, `id_kendaraan`, `id_owner`) VALUES
+(1, '2026-06-18', 'Ditolak', 2, 8, NULL),
+(2, '2026-06-16', 'Selesai', 2, 8, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `testdrive`
+--
+
+CREATE TABLE `testdrive` (
+  `id_testdrive` bigint NOT NULL,
+  `tanggal` date NOT NULL,
+  `jam` time NOT NULL,
+  `status` varchar(50) NOT NULL,
+  `catatan` text,
+  `id_pembeli` bigint NOT NULL,
+  `id_kendaraan` bigint NOT NULL,
+  `id_owner` bigint DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `testdrive`
+--
+
+INSERT INTO `testdrive` (`id_testdrive`, `tanggal`, `jam`, `status`, `catatan`, `id_pembeli`, `id_kendaraan`, `id_owner`) VALUES
+(1, '2026-06-26', '21:09:00', 'Selesai', 'lajdnwk', 2, 8, NULL),
+(2, '2026-06-18', '22:14:00', 'Ditolak', 'ljmk', 2, 8, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` bigint NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id_user`, `nama`, `username`, `email`, `password`, `role`) VALUES
+(2, 'jopan', 'jopanbekti', 'jopan@gmail.com', '1234567890', 'Pembeli'),
+(3, 'jopan', 'kreezp', 'nivea@hahaha.digital', '1234567890', 'Pembeli'),
+(4, 'Admin Owner', 'adminowner', 'owner@autoprime.id', '12345', 'Owner');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `kendaraan`
+--
+ALTER TABLE `kendaraan`
+  ADD PRIMARY KEY (`id_kendaraan`);
+
+--
+-- Indexes for table `mobil`
+--
+ALTER TABLE `mobil`
+  ADD PRIMARY KEY (`id_mobil`),
+  ADD KEY `fk_mobil_kendaraan` (`id_kendaraan`);
+
+--
+-- Indexes for table `motor`
+--
+ALTER TABLE `motor`
+  ADD PRIMARY KEY (`id_motor`),
+  ADD KEY `fk_motor_kendaraan` (`id_kendaraan`);
+
+--
+-- Indexes for table `owner`
+--
+ALTER TABLE `owner`
+  ADD PRIMARY KEY (`id_owner`),
+  ADD KEY `fk_owner_user` (`id_user`);
+
+--
+-- Indexes for table `pembeli`
+--
+ALTER TABLE `pembeli`
+  ADD PRIMARY KEY (`id_pembeli`),
+  ADD KEY `fk_pembeli_user` (`id_user`);
+
+--
+-- Indexes for table `penjualan`
+--
+ALTER TABLE `penjualan`
+  ADD PRIMARY KEY (`id_penjualan`),
+  ADD KEY `fk_penjualan_pembeli` (`id_pembeli`),
+  ADD KEY `fk_penjualan_kendaraan` (`id_kendaraan`),
+  ADD KEY `fk_penjualan_owner` (`id_owner`);
+
+--
+-- Indexes for table `testdrive`
+--
+ALTER TABLE `testdrive`
+  ADD PRIMARY KEY (`id_testdrive`),
+  ADD KEY `fk_testdrive_pembeli` (`id_pembeli`),
+  ADD KEY `fk_testdrive_kendaraan` (`id_kendaraan`),
+  ADD KEY `fk_testdrive_owner` (`id_owner`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `kendaraan`
+--
+ALTER TABLE `kendaraan`
+  MODIFY `id_kendaraan` bigint NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `mobil`
+--
+ALTER TABLE `mobil`
+  MODIFY `id_mobil` bigint NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `motor`
+--
+ALTER TABLE `motor`
+  MODIFY `id_motor` bigint NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `owner`
+--
+ALTER TABLE `owner`
+  MODIFY `id_owner` bigint NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `pembeli`
+--
+ALTER TABLE `pembeli`
+  MODIFY `id_pembeli` bigint NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `penjualan`
+--
+ALTER TABLE `penjualan`
+  MODIFY `id_penjualan` bigint NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `testdrive`
+--
+ALTER TABLE `testdrive`
+  MODIFY `id_testdrive` bigint NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` bigint NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `mobil`
+--
+ALTER TABLE `mobil`
+  ADD CONSTRAINT `fk_mobil_kendaraan` FOREIGN KEY (`id_kendaraan`) REFERENCES `kendaraan` (`id_kendaraan`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `motor`
+--
+ALTER TABLE `motor`
+  ADD CONSTRAINT `fk_motor_kendaraan` FOREIGN KEY (`id_kendaraan`) REFERENCES `kendaraan` (`id_kendaraan`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `owner`
+--
+ALTER TABLE `owner`
+  ADD CONSTRAINT `fk_owner_user` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `pembeli`
+--
+ALTER TABLE `pembeli`
+  ADD CONSTRAINT `fk_pembeli_user` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `penjualan`
+--
+ALTER TABLE `penjualan`
+  ADD CONSTRAINT `fk_penjualan_kendaraan` FOREIGN KEY (`id_kendaraan`) REFERENCES `kendaraan` (`id_kendaraan`),
+  ADD CONSTRAINT `fk_penjualan_owner` FOREIGN KEY (`id_owner`) REFERENCES `owner` (`id_owner`),
+  ADD CONSTRAINT `fk_penjualan_pembeli` FOREIGN KEY (`id_pembeli`) REFERENCES `pembeli` (`id_pembeli`);
+
+--
+-- Constraints for table `testdrive`
+--
+ALTER TABLE `testdrive`
+  ADD CONSTRAINT `fk_testdrive_kendaraan` FOREIGN KEY (`id_kendaraan`) REFERENCES `kendaraan` (`id_kendaraan`),
+  ADD CONSTRAINT `fk_testdrive_owner` FOREIGN KEY (`id_owner`) REFERENCES `owner` (`id_owner`),
+  ADD CONSTRAINT `fk_testdrive_pembeli` FOREIGN KEY (`id_pembeli`) REFERENCES `pembeli` (`id_pembeli`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
