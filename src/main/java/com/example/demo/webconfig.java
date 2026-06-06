492d5b5ab0d32c40ec491637531b4c2713654548
yanggatzy <yunusanggara06@gmail.com>
2026-06-06 15:43:06 +0700

    Beli Tempe Disini

--- src/main/java/com/example/demo/webconfig.java
+++ src/main/java/com/example/demo/webconfig.java
@@ -1,14 +1,14 @@
 package com.example.demo; // Sesuaikan dengan nama package utamamu!
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 @Configuration
-public class webconfig implements WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
