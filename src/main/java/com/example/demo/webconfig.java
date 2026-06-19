7524255d53d6fb99ef57ae68ffed457157e3a4e7
yanggatzy <yunusanggara06@gmail.com>
2026-06-20 03:23:01 +0700

    webconfig

--- src/main/java/com/example/demo/webconfig.java
+++ src/main/java/com/example/demo/webconfig.java
@@ -5,7 +5,7 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class Webconfig implements WebMvcConfigurer {
+public class webconfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
